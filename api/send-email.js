3c40071c970b4e9b4ea0f9a9f18f6e38964f51a8
Aksel Ozer <akselozer9@unm.edu>
2023-09-09 13:21:30 +0300

    fdh

--- api/send-email.js
+++ api/send-email.js
@@ -1,11 +1,4 @@
-const express = require('express');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
 
 const transporter = nodemailer.createTransport({
   service: 'hotmail.com',
@@ -15,31 +8,31 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-//email post route
-app.post('/send-email', (req, res) => {
-    console.log(req.body); 
-    const { pdfData } = req.body;
-
-  const mailOptions = {
-    from: 'akselozer9@hotmail.com',
-    to: 'ozeraksel@gmail.com',
-    subject: 'Form Submission',
-    text: 'Gridwatch Facility Assessment',
-    attachments: [
-      {
-        filename: 'form-data.pdf',
-        content: pdfData,
-        encoding: 'base64',
-      },
-    ],
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).send(error.toString());
+module.exports = (req, res) => {
+    if (req.method === 'POST') {
+      const { pdfData } = req.body;
+  
+      const mailOptions = {
+        from: 'akselozer9@hotmail.com',
+        to: 'ozeraksel@gmail.com',
+        subject: 'Form Submission',
+        text: 'Gridwatch Facility Assessment',
+        attachments: [
+          {
+            filename: 'form-data.pdf',
+            content: pdfData,
+            encoding: 'base64',
+          },
+        ],
+      };
+  
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+      });
+    } else {
+      res.status(405).send('Method not allowed');  // Handle non-POST requests
     }
-    res.status(200).send('Email sent: ' + info.response);
-  });
-});
-
-module.exports = app;
+  };
