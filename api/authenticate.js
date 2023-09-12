f2caad0b5bf90a8fe2213b373885042761f87ca9
Aksel Ozer <akselozer9@unm.edu>
2023-09-12 18:08:38 +0300

    “fix3”

--- api/authenticate.js
+++ api/authenticate.js
@@ -5,6 +5,8 @@ module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
   
     if (req.method === 'OPTIONS') {
       res.status(200).send('OK');
@@ -27,11 +29,11 @@ module.exports = (req, res) => {
   
       if (user) {
         const jwtToken = jwt.sign(
-          {id: user.id, email: user.username },
+          { id: user.id, email: user.email },  // Changed user.username to user.email
           'b51255d1d1ba41d0d751fe3e11609d26ae6d4459ba8a415e3a67ada63a67e57d'
-          )
-          console.log(jwtToken);
-
+        )
+        console.log(jwtToken);
+      
         res.status(200).send({ message: "Welcome Back!", token: jwtToken });
       } else {
         res.status(400).send({ message: 'Invalid email or password' });
