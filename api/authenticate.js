1835fdc63eec3bdbc5b1fe735188b5a732983da2
Aksel Ozer <akselozer9@unm.edu>
2023-09-24 23:21:18 +0300

    hasing added2

--- api/authenticate.js
+++ api/authenticate.js
@@ -8,7 +8,8 @@ module.exports = async (req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   if (req.method === 'OPTIONS') {
     res.status(200).send('OK');
